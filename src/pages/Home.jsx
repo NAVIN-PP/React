ca06217dae3d238c8827513408a45edb0d94698c
NAVIN-PP <navinputhiyapurayil123@gmail.com>
2023-07-05 16:24:07 +0530

    spoti

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import image from '../assets/spoti.png'
+import { Link } from 'react-router-dom'
+import './Home.css'
+import { renderMatches } from 'react-router-dom'
 
 const Home = () => {
     return (
         <>
+
             <div className="container">
                 <div className="img">
                     <img src={image} />
@@ -11,16 +15,22 @@ const Home = () => {
                 <div className="pr">Premium</div>
                 <div className="he">Help</div>
                 <div className="do">Download</div>
+                <div className="br">|</div>
                 <div className="su">Sign up</div>
-                <div className="li">Log in</div>
-                <div className="pi">navin</div>
+                <Link to="/Login" id='cg'>
+                    <div className="li">Log in</div>
+                </Link>
+
 
             </div>
             <div className="container1">
                 <div className="me">Music for everyone.</div>
-                <div className="mi">Millions of songs.No credit card needed.</div>
-                <button>GET SPOTIFY FREE</button>
+                <div className="mi">Millions of songs.No credit card needed.</div><br />
+                <div className="gs">
+                    <button>GET SPOTIFY FREE</button>
+                </div>
             </div>
+
         </>
     )
 }
